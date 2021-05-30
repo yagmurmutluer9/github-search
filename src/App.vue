06cc44abecd3edb30c0e6029dc4a6dbc9e4cf843
yagmurmutluer9 <yagmurmutluer9@gmail.com>
2021-05-30 15:52:03 +0300

    initialcommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/Octocat.png">
+    <Github/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Github from "./views/GitHub.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Github
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app img{
+  width: 100px;
 }
 </style>
